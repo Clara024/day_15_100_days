1d9a21c024aaa68770396dbeb4639a418b1dd687
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-12-02 23:54:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+exit = ""
+while exit != "yes":
+  animal= (input(" whats your favourite animal: cat,dog or lion?"))
+  if animal == "cat" or input == "Cat":
+    print ("A cat goes meow meow 🐈")
+  elif animal ==  "dog" or input == "Dog":
+    print (" A dog goes woof woof 🐕‍🦺")
+  elif animal == "lion" or input == "Lion":
+    print (" The lion goes roar roar 🦁")
+  else:
+    print ("animal not recognised")
+  exit = input("do you wish to exit the game?")
+  if exit == "yes" or exit == "Yes":
+    print ("Thank youu, I hope to see you soon!")
